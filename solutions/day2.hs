16ef77d0424a6323ab52bde5a49cc612180dca76
Wollam <w.a.lutton@students.uu.nl>
2024-12-02 12:47:03 +0100

    part2 solved

--- solutions/day2.hs
+++ solutions/day2.hs
@@ -35,53 +35,12 @@ checkDifference (x:xs:ys) = (diff >= 1 && diff <= 3) && checkDifference (xs:ys)
 checkDifference _ = True
 
 checkSafe2 :: String -> Bool
-checkSafe2 input = let
-    vals = map read (words input) :: [Int]
-    in checkSequence vals
+checkSafe2 input = 
+    let vals = map read (words input) :: [Int]
+    in checkSort vals && checkDifference vals || checkWithRemoval vals
 
-checkSequence :: [Int] -> Bool
-checkSequence xs = let
-    ascend = oddAscending xs
-    descend = oddDescending xs
-    difference = oddDifference xs
-    sortList = if length ascend > length descend then descend else ascend
-    in checkLists xs sortList difference
+checkWithRemoval :: [Int] -> Bool
+checkWithRemoval xs = any (\i -> let xs' = removeAt i xs in checkSort xs' && checkDifference xs') [0..length xs - 1]
+  where
+    removeAt i xs = take i xs ++ drop (i + 1) xs
 
-checkLists :: [Int] -> [Int] -> [Int] -> Bool
-checkLists xs sortList difference
-  | null sortList && null difference = True
-  | length sortList > 1 || length difference > 1 = False
-  | not (null sortList || null difference) = sortList == difference
-  | null sortList = checkNewList xs difference
-  | otherwise = checkNewList xs sortList
-
-checkNewList :: [Int] -> [Int] -> Bool
-checkNewList xs removevals = checkSort checkList && checkDifference checkList
-    where
-        checkList =  xs \\ removevals
-
-oddAscending :: [Int] -> [Int]
-oddAscending (x:xs:ys) = if x <= xs
-    then oddAscending (xs:ys)
-    else xs : oddAscending (xs:ys)
-oddAscending _ = []
-
-oddDescending :: [Int] -> [Int]
-oddDescending (x:xs:ys) = if x >= xs
-    then oddDescending (xs:ys)
-    else xs : oddDescending (xs:ys)
-oddDescending _ = []
-
-oddDifference :: [Int] -> [Int]
-oddDifference (x:xs:ys) = if diff >= 1 && diff <= 3
-    then oddDifference (xs:ys)
-    else x : oddDifference (xs:ys)
-    where
-        diff = abs (xs - x)
-oddDifference _ = []
-
-test2 = do
-    let example = [31, 32, 30, 33, 34, 37] 
-    print (oddAscending example)
-    print (oddDescending example)
-   
