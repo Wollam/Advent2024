01b869f1e939b14223899e3f376c0ac8410659ac
Wollam <w.a.lutton@students.uu.nl>
2024-12-02 01:20:19 +0100

    Finished part 2 of day1

--- solutions/day1.hs
+++ solutions/day1.hs
@@ -1,7 +1,7 @@
 module Day1 where
 
 import Data.List (foldr, sort)
-import Data.Map (Map, empty, insertWith, lookup)
+import Data.Map as Map (Map, empty, insertWith, lookup) 
 
 day1 :: IO ()
 day1 = do
@@ -9,7 +9,7 @@ day1 = do
     let rows = lines input
     let answer1 = part1 rows
     let answer2 = part2 rows
-    print answer1
+    print answer2
 
 part1 :: [String] -> Int
 part1 input = let 
@@ -33,11 +33,18 @@ countTogether = foldr f 0
 part2 :: [String] -> Int
 part2 input = let
     (listL, listR) = splitInput input
-
-    in 5
+    rightOccurences = countOccurences listR
+    in compareLists listL rightOccurences
 
 countOccurences :: [String] -> Map Int Int 
 countOccurences = foldr f e
     where
         f x = insertWith (+) (read x) 1 
         e = empty
+
+compareLists :: [String] -> Map Int Int -> Int
+compareLists l m = foldr f 0 l
+    where
+        f x acc = case Map.lookup (read x) m of
+            Nothing -> acc 
+            Just val -> acc + val * read x
