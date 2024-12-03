9cc885fc812670642d172dd276f3dca087080c9f
Wollam <w.a.lutton@students.uu.nl>
2024-12-03 08:21:30 +0100

    day3 part 2 solved

--- solutions/day3.hs
+++ solutions/day3.hs
@@ -9,7 +9,8 @@ day3 :: IO ()
 day3 = do
     input <- readFile "inputs/day3.txt"
     let answer1 = calculateTotal (matchRegex input)
-    print answer1
+    let answer2 = calculateTotal2 (matchRegex2 input) True
+    print answer2
 
 matchRegex :: String -> [String]
 matchRegex input = getAllTextMatches (input =~ regex)
@@ -31,3 +32,17 @@ extractIntegers input =
                 let (a, b) = break (== ',') inside
                 in (read a, read (tail b))
         _ -> error "No opening parenthesis found"
+
+matchRegex2 :: String -> [String]
+matchRegex2 input = getAllTextMatches (input =~ regex)
+    where
+        regex = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)"
+
+calculateTotal2 :: [String] -> Bool -> Int
+calculateTotal2 [] _ = 0
+calculateTotal2 (x:xs) enabled = case x of
+    "do()" -> calculateTotal2 xs True
+    "don't()" -> calculateTotal2 xs False
+    _ -> if enabled then l * r + calculateTotal2 xs enabled else calculateTotal2 xs enabled
+        where
+            (l, r) = extractIntegers x
